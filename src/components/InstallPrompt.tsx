47b927d75ec096e9f157bf025eb29a302c764ab9
SN <sn@MacBook-Pro.local>
2025-03-01 01:33:32 +0800

    update

--- src/components/InstallPrompt.tsx
+++ src/components/InstallPrompt.tsx
@@ -32,13 +32,11 @@ export default function InstallPrompt() {
           <p>
             To install this app on your iOS device, tap the share button
             <span role="img" aria-label="share icon">
-              {' '}
-              ⎋{' '}
+              ⎋
             </span>
-            and then "Add to Home Screen"
+            and then Add to Home Screen
             <span role="img" aria-label="plus icon">
-              {' '}
-              ➕{' '}
+              ➕
             </span>.
           </p>
         )}
