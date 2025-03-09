456ab09bf415b2f7bb36a1879a6b49766380ab45
SN <sn@MacBook-Pro.local>
2025-03-09 21:58:18 +0800

    update version 1.0.23 ok

--- src/app/api/domain/route.ts
+++ src/app/api/domain/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { version } from 'os';
 
 export async function GET(request: NextRequest) {
   const protocol = request.headers.get('x-forwarded-proto') || 'http';
   const host = request.headers.get('host');
   const currentDomain = `${protocol}://${host}`;
 
-  return NextResponse.json({ currentDomain });
+  return NextResponse.json({ currentDomain, api: process.env.API_DOMAIN, version: process.env.APP_VERSION });
 }
---
+++ src/app/mobile/page.tsx
@@ -0,0 +1,7 @@
+export default function MobilePage() { 
+    return (
+        <div>
+            <h1>Mobile Page</h1>
+        </div>
+    )
+}
